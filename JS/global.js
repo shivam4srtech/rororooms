97413e5aaf523e69c62b6ade21083a732d47e770
shivam4srtech <shivam.srtech@gmail.com>
2025-02-15 18:37:03 +0530

    deployed

--- JS/global.js
+++ JS/global.js
@@ -4,22 +4,17 @@
 
         links.forEach(link => {
             link.addEventListener("click", function (event) {
-                event.preventDefault(); // Prevent default anchor action
-
-                // Close all floatingBoxes
+                event.preventDefault(); 
                 document.querySelectorAll(".floatingBoxes").forEach(box => {
                     box.style.display = "none";
                 });
 
-                // Show the corresponding floatingBoxes div
                 const floatingBox = this.nextElementSibling;
                 if (floatingBox.classList.contains("floatingBoxes")) {
                     floatingBox.style.display = "block";
                 }
             });
         });
-
-        // Close the box if clicking outside
         document.addEventListener("click", function (event) {
             if (!event.target.closest(".navLinkBox li")) {
                 document.querySelectorAll(".floatingBoxes").forEach(box => {
