90d1f21134d9ac94b993d2b69ca2b96f6f9eaf84
shivam4srtech <shivam.srtech@gmail.com>
2025-02-15 18:31:26 +0530

    first commit

--- JS/global.js
+++ JS/global.js
@@ -1,40 +1,30 @@
-const nav = document.querySelector(".nav"),
-    searchIcon = document.querySelector("#searchIcon"),
-    navOpenBtn = document.querySelector(".navOpenBtn"),
-    navCloseBtn = document.querySelector(".navCloseBtn");
 
-searchIcon.addEventListener("click", () => {
-    nav.classList.toggle("openSearch");
-    nav.classList.remove("openNav");
-    if(nav.classList.contains("openSearch")){
-        return searchIcon.classList.replace("fa-search","fa-times");
-    }
-     searchIcon.classList.replace("fa-times","fa-search");
-});
-navOpenBtn.addEventListener("click", () => {
-    nav.classList.add("openNav");
-    nav.classList.remove("openSearch");
-    searchIcon.classList.replace("fa-times","fa-search");
-    
-});
-navCloseBtn.addEventListener("click", () => {
-    nav.classList.remove("openNav");
-    
-});
-// ===============Dropdown expand on hover============
-$( document ).ready(function() {
+    document.addEventListener("DOMContentLoaded", function () {
+        const links = document.querySelectorAll(".displayFloatingBox");
 
-    // Add hover action for dropdowns
-    let dropdown_hover = $(".dropdown-hover");
-    dropdown_hover.on('mouseover', function(){
-        let menu = $(this).find('.dropdown-menu'), toggle = $(this).find('.dropdown-toggle');
-        menu.addClass('show');
-        toggle.addClass('show').attr('aria-expanded', true);
+        links.forEach(link => {
+            link.addEventListener("click", function (event) {
+                event.preventDefault(); // Prevent default anchor action
+
+                // Close all floatingBoxes
+                document.querySelectorAll(".floatingBoxes").forEach(box => {
+                    box.style.display = "none";
+                });
+
+                // Show the corresponding floatingBoxes div
+                const floatingBox = this.nextElementSibling;
+                if (floatingBox.classList.contains("floatingBoxes")) {
+                    floatingBox.style.display = "block";
+                }
+            });
+        });
+
+        // Close the box if clicking outside
+        document.addEventListener("click", function (event) {
+            if (!event.target.closest(".navLinkBox li")) {
+                document.querySelectorAll(".floatingBoxes").forEach(box => {
+                    box.style.display = "none";
+                });
+            }
+        });
     });
-    dropdown_hover.on('mouseout', function(){
-        let menu = $(this).find('.dropdown-menu'), toggle = $(this).find('.dropdown-toggle');
-        menu.removeClass('show');
-        toggle.removeClass('show').attr('aria-expanded', false);
-    });
-
-});
